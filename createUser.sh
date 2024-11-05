2737dc09693ab34a2e4a01cc2e70ba7b20703a7d
tia-porota <cafesinleche935@gmail.com>
2024-11-04 21:23:37 -0300

    English translation + readme

--- createUser.sh
+++ createUser.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 echo @@@@@@@@@@@@@@@@@@@@@
-echo + User Creator v0.4 +
+echo + User Creator v0.44 +
 echo @@@@@@@@@@@@@@@@@@@@@
 echo
 echo
 
-echo Bienvenide, ingrese la opción adecuada
-echo "1. Usuario permanente"
-echo "2. Usuario temporal (x días)"
+echo Welcome, please select your desired option
+echo "1. Permanent User"
+echo "2. Temp User (n days)"
 echo
 
 read option
 
 case $option in
   1)
-	read -p "Ingrese el nombre de usuario a crear: " username
+	read -p "Enter the new users name: " username
 	sudo useradd $username
-	echo Usuario $username creado con éxito
+	echo User $username created succesfully
 	;;
   2)
-	read -p "Ingrese el nombre de usuario a crear: " username
+	read -p "Enter the new users name: " username
 	
 	
-	read -p "Ingrese la cantidad de días en el que el usuario expirará: " days
+	read -p "Enter how many days the user will be available: " days
 	expiredate=$( date -d "+$days days" -u +%Y-%m-%d )
 	
 	
 	sudo useradd $username -e $expiredate
-	echo Usuario $username creado con éxito, expirará el $expiredate
+	echo User $username created succesfully, it will expire on $expiredate
 	;;
   *)
-	echo "Opción Inválida"
+	echo "Invalid Option"
 esac
 
 
-read -sp "Presione enter para salir"
+read -sp "Press 'Enter' to exit"
 
 
